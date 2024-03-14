fd9fb4b2dec0d7c90b4ebdc7ead30df27bc63ac8
edsu7 <22638361+edsu7@users.noreply.github.com>
2024-03-14 16:11:46 -0400

    first commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,4 +1,10 @@
 // THIS IS A SAMPLE SCHEMA
+import study from "./study.json" assert { type: "json" };
+import participant from "./participant.json" assert { type: "json" };
 import sample from "./sample.json" assert { type: "json" };
+import specimen from "./specimen.json" assert { type: "json" };
 
+export default { study };
+export default { participant };
 export default { sample };
+export default { specimen };
